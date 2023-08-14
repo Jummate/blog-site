ac5ed8e913a8762ca7c6dadf6ba8a7cd3bdd4b6f
Jummate <yakubjumat@gmail.com>
2023-08-14 12:03:23 +0100

    chore: isolate posts data into a separate file for dynamic rendering

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import IMAGE_TWO from "./assets/image2.jpg";
+import IMAGE_ONE from "./assets/image1.jpg";
 
 export const mobileMenuData = {
   "Main Menu": {
@@ -28,4 +29,32 @@ export const topicData = {
   Nature: IMAGE_TWO,
   Technology: IMAGE_TWO,
 };
+
+export const postData = [
+  {
+    id: "qaAb23456ZCVhjk",
+    tag: "Science",
+    title: "My First Foray Into Lazy Loading With JavaScript",
+    summary:
+      " Lazy Loading is a performance-improvement technique which seeks to lorem ipsum when the lorem ipsum is the lorem ipsum within the lorem ipsum...",
+    readTime: "4min",
+    author: "Olawale Jumat",
+    createdAt: "April 20, 2023",
+    bannerImage: IMAGE_ONE,
+    authorImage: IMAGE_ONE,
+  },
+
+  {
+    id: "xaAb109836ZCVzct",
+    tag: "Nature",
+    title: "My First Foray Into Nature",
+    summary:
+      " Lazy Loading is a performance-improvement technique which seeks to lorem ipsum when the lorem ipsum is the lorem ipsum within the lorem ipsum...",
+    readTime: "6min",
+    author: "Olawale Jumat",
+    createdAt: "April 20, 2023",
+    bannerImage: IMAGE_TWO,
+    authorImage: IMAGE_TWO,
+  },
+];
 // module.exports = { mobileMenuData, tagData };
