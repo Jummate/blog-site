8aaa91e456a51b95795bfc61211318c369a91f67
Jummate <yakubjumat@gmail.com>
2023-08-14 16:29:25 +0100

    feat: add react-quill rich text editor to Create page

--- client/src/pages/create/Create.jsx
+++ client/src/pages/create/Create.jsx
@@ -1,7 +1,19 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
 const CreatePost = () => {
   return (
     <section>
-      <h1>Create a new post here.</h1>
+      <form>
+        <Input placeholder={"Enter the title"} />
+        <Input placeholder={"Enter the summary"} />
+        <Input
+          type={"file"}
+          placeholder={"Enter the summary"}
+        />
+        <ReactQuill theme="snow" />
+      </form>
     </section>
   );
 };
