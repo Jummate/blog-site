ad65f7364d9fd5795554dba945601cb749b4e166
Jummate <yakubjumat@gmail.com>
2023-08-23 16:29:22 +0100

    chore: make Hero responsive and update with dummy image

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,10 +1,11 @@
 import Button from "../button/Button";
 import Input from "../input/Input";
+import IMAGE_ONE from "../../assets/image1.jpg";
 
 const Hero = () => {
   return (
-    <section className="bg-slate-100 text-sky-900 dark:bg-sky-900 dark:text-slate-100 p-10">
-      <div className="flex flex-col items-center justify-center gap-5">
+    <section className="bg-slate-100 text-sky-900 dark:bg-sky-900 dark:text-slate-100 p-10 md:flex gap-5 justify-center items-center">
+      <div className="flex flex-col items-center justify-center gap-5 py-5">
         <h1 className="text-2xl">
           Hi, I'm <span className="font-bold text-3xl">Olawale Jumat</span>
         </h1>
@@ -23,7 +24,13 @@ const Hero = () => {
           />
         </div>
       </div>
-      <div className="hidden md:block">hffhfhfhh</div>
+      <div className="hidden md:flex justify-center items-center max-w-screen-sm">
+        <img
+          src={IMAGE_ONE}
+          alt=""
+          className="w-3/4 h-auto"
+        />
+      </div>
     </section>
   );
 };
