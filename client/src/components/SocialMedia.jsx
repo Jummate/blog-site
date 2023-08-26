0227af525ff30473c4fff2abc6de424bd7d8bf68
Jummate <yakubjumat@gmail.com>
2023-08-26 01:22:07 +0100

    refactor: isolate social media icons in footer to a separate component

---
+++ client/src/components/SocialMedia.jsx
@@ -0,0 +1,31 @@
+import { socialMedia } from "../data";
+
+import { FaLinkedin, FaGlobe, FaTwitter } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+
+const icons = {
+  linkedin: <FaLinkedin />,
+  website: <FaGlobe />,
+  email: <BiLogoGmail />,
+  twitter: <FaTwitter />,
+};
+
+const SocialMedia = ({ userType }) => {
+  const data = socialMedia[`${userType}`];
+  return (
+    <ul className="flex gap-5 text-2xl">
+      {Object.keys(data).map((item, index) => (
+        <li key={index}>
+          <a
+            href={data[`${item}`].href}
+            title={data[`${item}`].title}
+          >
+            {icons[item]}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SocialMedia;
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,8 +1,7 @@
-import { FaLinkedin, FaGlobe } from "react-icons/fa";
-import { BiLogoGmail } from "react-icons/bi";
 import IMAGE_THREE from "../../assets/image3.jpg";
 import Input from "../input/Input";
 import Button from "../button/Button";
+import SocialMedia from "../SocialMedia";
 const Footer = () => {
   return (
     <footer className="text-sky-100 p-5 pt-10 bg-sky-900 flex items-center justify-center">
@@ -30,72 +29,14 @@ const Footer = () => {
           ></img>
           <h1 className="text-xl font-bold">Olawale Jumat</h1>
           <h3 className="text-md mb-3">Content Writer</h3>
-          <ul className="flex gap-5 text-2xl">
-            <li>
-              {" "}
-              <a
-                href="https://www.linkedin.com/in/omololu-jumat-1405"
-                title="Developer's LinkedIn Profile"
-              >
-                <FaLinkedin />
-              </a>
-            </li>
-
-            <li>
-              {" "}
-              <a
-                href="https://omololujumat.netlify.app"
-                title="Developer's Portfolio Website"
-              >
-                <FaGlobe />
-              </a>
-            </li>
-            <li>
-              {" "}
-              <a
-                href="mailto:omololujumat@gmail.com"
-                title="Developer's Email Address"
-              >
-                <BiLogoGmail />
-              </a>
-            </li>
-          </ul>
+          <SocialMedia userType="user" />
         </section>
 
         <section className="flex flex-col justify-center gap-5 items-center opacity-60 text-center">
           <span className="text-md leading-8">
             Designed & Built By <b>Omololu Jumat</b>
           </span>
-          <ul className="flex gap-3 text-2xl">
-            <li>
-              {" "}
-              <a
-                href="https://www.linkedin.com/in/omololu-jumat-1405"
-                title="Developer's LinkedIn Profile"
-              >
-                <FaLinkedin />
-              </a>
-            </li>
-
-            <li>
-              {" "}
-              <a
-                href="https://omololujumat.netlify.app"
-                title="Developer's Portfolio Website"
-              >
-                <FaGlobe />
-              </a>
-            </li>
-            <li>
-              {" "}
-              <a
-                href="mailto:omololujumat@gmail.com"
-                title="Developer's Email Address"
-              >
-                <BiLogoGmail />
-              </a>
-            </li>
-          </ul>
+          <SocialMedia userType="developer" />
         </section>
       </div>
     </footer>
