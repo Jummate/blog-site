6e9c0059a95c9a5a3e85de7ec31b2a01932bbf41
Jummate <yakubjumat@gmail.com>
2023-09-06 12:28:08 +0100

    refactor: rework the nav links and items in Menu and Header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { FaBars } from "react-icons/fa";
 import Menu from "../menu/Menu";
 import ColorMode from "../ColorMode";
 import { Link } from "react-router-dom";
-import { menuData } from "../../data";
+import Button from "../button/Button";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const Header = () => {
+  const { token } = useContext(AuthContext);
   const [openMenu, setOpenMenu] = useState(false);
   const [colorMode, setColorMode] = useState(
     localStorage.hasOwnProperty("colorMode")
@@ -15,21 +17,38 @@ const Header = () => {
 
   useEffect(() => localStorage.setItem("colorMode", colorMode), [colorMode]);
 
-  const [mainMenu, ,] = Object.values(menuData);
   return (
     <header className="w-full p-5 md:px-10 font-sans font-bold sticky top-0 left-0 bg-slate-100 text-sky-900 dark:bg-sky-900 dark:text-slate-100 z-10">
-      <nav className="flex justify-between">
+      <nav className="flex justify-between items-center">
         <h1 className="cursor-pointer"> Leo's Blog</h1>
-        <div className="hidden md:flex md:text-md gap-6">
-          {Object.keys(mainMenu).map((item, index) => (
+        <div className="hidden md:flex md:text-md gap-6 items-center">
+          <Link
+            to="/"
+            className="hover:underline"
+          >
+            Home
+          </Link>
+          {!token && (
             <Link
-              key={index}
-              to={mainMenu[`${item}`].link}
+              to="login"
               className="hover:underline"
             >
-              {item}
+              Log In
             </Link>
-          ))}
+          )}
+          {token && (
+            <Link
+              to="create"
+              className="hover:underline"
+            >
+              Create New Post
+            </Link>
+          )}
+          {token && (
+            <Button extraStyles="shadow-pref bg-sky-900 dark:bg-sky-600 dark:text-slate-50">
+              Log Out
+            </Button>
+          )}
         </div>
         <div className="flex gap-3">
           <ColorMode
--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,10 +1,14 @@
 import { FaTimes } from "react-icons/fa";
-import { menuData } from "../../data";
+// import { menuData } from "../../data";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
+import { useContext } from "react";
+import Button from "../button/Button";
 
 const Menu = ({ onClick }) => {
+  const { token } = useContext(AuthContext);
   return (
-    <nav className=" md:hidden fixed z-10 font-sans flex flex-col gap-5 w-full min-h-screen px-5 top-0 left-0 bg-sky-100 text-sky-600 dark:bg-sky-800 dark:text-sky-100 py-5">
+    <nav className="md:hidden fixed z-10 font-sans flex flex-col gap-5 w-full min-h-screen px-5 top-0 left-0 bg-sky-100 text-sky-600 dark:bg-sky-800 dark:text-sky-100 py-5">
       <div className="flex justify-end text-2xl gap-3">
         {/* {ColorMode} */}
         <FaTimes
@@ -12,7 +16,39 @@ const Menu = ({ onClick }) => {
           onClick={onClick}
         />
       </div>
-      {Object.keys(menuData).map((heading, index) => (
+      <div className="flex flex-col gap-6 items-center">
+        <h3 className="text-lg mb-2 text-sky-800 dark:text-sky-200">
+          Main Menu
+        </h3>
+        <Link
+          to="/"
+          className="hover:underline"
+        >
+          Home
+        </Link>
+        {!token && (
+          <Link
+            to="login"
+            className="hover:underline"
+          >
+            Log In
+          </Link>
+        )}
+        {token && (
+          <Link
+            to="create"
+            className="hover:underline"
+          >
+            Create New Post
+          </Link>
+        )}
+        {token && (
+          <Button extraStyles="shadow-pref bg-sky-900 dark:bg-sky-600 dark:text-slate-50 px-10">
+            Log Out
+          </Button>
+        )}
+      </div>
+      {/* {Object.keys(menuData).map((heading, index) => (
         <div
           key={index}
           className="flex flex-col gap-2"
@@ -34,7 +70,7 @@ const Menu = ({ onClick }) => {
             <hr className="mt-3 border-sky-800/20 dark:border-sky-200/20 border-2" />
           ) : null}
         </div>
-      ))}
+      ))} */}
     </nav>
   );
 };
