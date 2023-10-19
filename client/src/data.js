fa88263f88901d4babbb3bf1647d7aab97ec19cb
Jummate <yakubjumat@gmail.com>
2023-10-19 11:35:32 +0100

    chore: update footer with owner's info

--- client/src/data.js
+++ client/src/data.js
@@ -1,21 +1,9 @@
 export const profile = {
   user: {
     fullName: "Olawale Jumat",
-    linkedin: {
-      href: "https://www.linkedin.com/in/omololu-jumat-1405",
-      title: "Developer's LinkedIn Profile",
-    },
-    website: {
-      href: "https://omololujumat.netlify.app",
-      title: "Developer's Portfolio Website",
-    },
     email: {
-      href: "mailto:omololujumat@gmail.com",
-      title: "Developer's Email Address",
-    },
-    twitter: {
-      href: "mailto:omololujumat@gmail.com",
-      title: "Developer's Email Address",
+      href: "mailto:olawalejumat43@gmail.com",
+      title: "Owner's Email Address",
     },
   },
 
