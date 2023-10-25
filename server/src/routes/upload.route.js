091dbfd2cc17e060a81d8b9830afa7ba8076a00a
Jummate <yakubjumat@gmail.com>
2023-10-25 09:38:28 +0100

    fix: broken link

--- server/src/routes/upload.route.js
+++ server/src/routes/upload.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const uploadController = require("./upload.controller");
+const uploadController = require("../controllers/upload.controller");
 const { uploadFiles } = require("../middleware/uploadFiles");
 
 router.route("/").post(uploadFiles, uploadController.uploadContentImage);
