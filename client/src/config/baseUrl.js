433784cee712f11ed5c77e75680b32c731c2df19
Jummate <yakubjumat@gmail.com>
2024-02-11 14:47:10 +0100

    chore: minor changes

--- client/src/config/baseUrl.js
+++ client/src/config/baseUrl.js
@@ -1,7 +1,7 @@
 const baseUrl = {
-  clientBaseUrlDev: "http://localhost:3600",
+  // clientBaseUrlDev: "http://localhost:3600",
   clientBaseUrl: "https://macroblog.onrender.com",
-  serverBaseUrlDev: "http://localhost:3500",
+  // serverBaseUrlDev: "http://localhost:3500",
   serverBaseUrl: "https://macroblog-api.onrender.com",
 };
 
